552203211d1aaf9b4fa30ba5a9947b35b3003122
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2025-10-10 14:32:11 +0100

    hide the side bar while updating the password

---
+++ app/auth/update-password/layout.tsx
@@ -0,0 +1,5 @@
+import { PropsWithChildren } from 'react';
+
+export default function UpdatePasswordLayout({ children }: PropsWithChildren) {
+  return <main className="min-h-screen">{children}</main>;
+}
